18eaf78c69d9c5fe3e854d4cd1dce221c31a2c8c
lokeshgajul <sangeetagajul@gmail.com>
2025-06-26 19:20:34 +0530

    connect to MongoDB on Vercel

--- DB/Db.js
+++ DB/Db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDb = async () => {
+const connectDB = async () => {
   try {
-    const dbUrl = await mongoose.connect(process.env.dbUrl);
-    console.log("Db Connected Successfully.. ");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
   }
 };
 
-export default connectDb;
+export default connectDB;
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDb from "./DB/Db.js";
 import routes from "./routes/index.js";
+import connectDB from "./DB/Db.js";
 dotenv.config();
-connectDb();
+connectDB();
+
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Allow your local frontend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these HTTP methods
-  credentials: true, // Allow cookies, authorization headers, etc.
-  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 200 for OPTIONS
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));

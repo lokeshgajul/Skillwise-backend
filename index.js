7544bf3842ae9a2f4d7d07c05da293478ffcce59
lokeshgajul <sangeetagajul@gmail.com>
2025-06-26 19:27:31 +0530

    pdate CORS to allow local IP access

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://192.168.0.104:5173"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
